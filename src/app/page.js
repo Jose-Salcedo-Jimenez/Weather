d260cc72e2457abb588f34ee203461bbdc7284ec
Jose Salcedo Jimenez <josesslcedo@gmail.com>
2025-03-19 13:34:46 -0500

    The Main Information component was implemented with an API call

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,18 @@ import { Header } from "@/layout/Header";
 import { MainInformation } from "@/ui/MainInformation";
 import { handleWeather, urlFormat } from "@/util/fetchWeather";
 
-export default async function Home({params}) {
+export default async function Home() {
 
   const url = urlFormat('Cartagena')
-  const data = await handleWeather(url)
-  const {temperature} = data.current;
+  const data = await handleWeather(url);
+  
+  console.log(data)
+  const {temperature, precipitation} = data.current;
   return (
     <>
       <Header />
       <main>
-        <MainInformation temperature={temperature} rain={temperature}/>
+        <MainInformation temperature={temperature} rain={precipitation.total}/>
       </main>
     </>
   );
--- src/ui/MainInformation.jsx
+++ src/ui/MainInformation.jsx
@@ -1,18 +1,34 @@
 import Image from "next/image"
-export const MainInformation = ({ temperature, rain, city }) => {
-    console.log(rain)
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ['latin'] })
+export const MainInformation = ({ temperature = 31, rain = 0, city = "Madrid" }) => {
     return (
-        <div className="mr-10 ml-10">
-            <div className="border border-black flex justify-center items-center flex-col"> 
+        <div className={`mr-10 ml-10 mt-4 ${inter.className}`}>
+            <div className="flex justify-center items-center flex-col">
                 <Image
                     src={`${temperature < 30 ? '/icons/cloudy.png' : '/icons/Sun.png'}`}
-                    width={170}
-                    height={170}
+                    width={180}
+                    height={180}
                     alt="Website weather"
                 />
-                <p className="font-medium text-[#373737]">Change of rain {rain} <span>{temperature}</span></p>
+                <div className="container tracking-wide flex flex-col w-full p-[0rem_0rem_0rem_3.2rem] ">
+                    <p className="text-5xl font-semibold text-gray-700">
+                        {city}
+                    </p>
+                    <div className="grid grid-cols-2 ">
+                        <div>
+                            <p className="mt-2 font-medium tracking-wide text-[#373737] text-sm">Change of rain {rain}%</p>
+                        </div>
+                        <div className="flex w-3/5 justify-end">
+                            <p className="text-4xl font-bold text-gray-700"><span>{Math.trunc(temperature)}°</span></p>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
-            
+
         </div>
     )
 }
